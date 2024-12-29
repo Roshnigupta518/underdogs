c340164cf8ec0e31e42d224f7b7d42d01fb85873
Roshnigupta518 <roshnigupta518@gmail.com>
2024-12-29 21:53:25 +0530

    change app name

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
  */
 
 import {AppRegistry, LogBox, Text, TextInput} from 'react-native';
-import App from './App';
-// import App from './src/App';
+// import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 Text.defaultProps = Text.defaultProps || {};
